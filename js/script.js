1fb117d0ca47f74ccf8e90470030cc1f97328340
marikomkheidze <110020576+marikomkheidze@users.noreply.github.com>
2022-09-07 18:32:05 +0400

    map

--- js/script.js
+++ js/script.js
@@ -1,23 +1,24 @@
 $('.slider').slick({
     infinite: true,
-    speed: 300,
+    autoplay: true,
+    autoplayspeed: 300,
+    speed: 500,
     slidesToShow: 3,
-    slidesToScroll: 1,
+    slidesToScroll: 2,
     responsive: [
         {
             breakpoint: 1024,
             settings: {
                 slidesToShow: 3,
-                slidesToScroll: 1,
+                slidesToScroll: 2,
                 infinite: true,
-                dots: true
             }
         },
         {
             breakpoint: 600,
             settings: {
-                slidesToShow: 2,
-                slidesToScroll: 2
+                slidesToShow: 1,
+                slidesToScroll: 1
             }
         },
         {
